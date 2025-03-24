be61ef4ce36f850ba983df5d058d955986a73f31
apoborka <alex.poborka@gmail.com>
2025-03-24 15:47:50 -0400

    Updated .env and Sequelize for Render hosting

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,9 +6,17 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true, // Enforce SSL for secure connections
+          rejectUnauthorized: false, // Allow self-signed certificates
+        },
+      },
+    })
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
